40b4a032d6030b0f0a8efd07fcf8f0072bc8dc63
Tunabel <tuan.23a5@gmail.com>
2020-10-06 21:33:50 +0700

    Project start

---
+++ src/main/java/c1812m/happyfeet/HappyfeetApplication.java
@@ -0,0 +1,13 @@
+package c1812m.happyfeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyfeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyfeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/c1812m/happyfeet/HappyfeetApplicationTests.java
@@ -0,0 +1,13 @@
+package c1812m.happyfeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyfeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
